9d4cbfc7cf60415653042e03df1c8b78d50f6509
mjmeilahn <mjmeilahn@gmail.com>
2019-08-05 17:37:03 -0700

    Refactor sendgrid

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,12 +1,10 @@
 const sgMail = require('@sendgrid/mail')
+// const apiKey
+// sgMail.setApiKey(apiKey)
 
-const sendgridAPIKey = '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'
-
-sgMail.setApiKey(sendgridAPIKey)
-
-sgMail.send({
-    to: 'xxxxxxx',
-    from: 'xxxxx',
-    subject: 'This is a test',
-    text: 'I am a test!'
-})
+// sgMail.send({
+//     to: 'xxxxxxx',
+//     from: 'xxxxx',
+//     subject: 'This is a test',
+//     text: 'I am a test!'
+// })
